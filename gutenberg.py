0adba965a806af6db0edf4a2a0642f4fb412c56a
AaronStonewins <appu.253@gmail.com>
2018-02-27 21:04:54 +0530

    Add files via upload

---
+++ gutenberg.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 26 00:14:57 2018
+
+@author: abhishek
+"""
+import NLUAssignment1 as nlu
+import math
+from nltk.corpus import gutenberg 
+import nltk
+
+
+x=gutenberg.sents()
+Tr=int(math.floor(len(x)*0.95))
+train=x[0:Tr]
+test=x[Tr:len(x)]
+
+N=3
+d={};dtemp={};L1=0
+for i in range(1,N+1):
+    dtemp=nlu.get_probability(train,i)[0]
+    if i==1:
+        L1=nlu.get_probability(train,i)[1]
+    for k,v in dtemp.items():
+        d[k]=v
+    
+F=nlu.perplexity(test,d,N,L1)
+print(F)
+
+
